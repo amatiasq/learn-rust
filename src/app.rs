7c7643eef96cb2f30a6e82de28d2847adb24e192
A. Matías Quezada <amatiasq@gmail.com>
2023-03-15 02:17:41 +0100

    I only know that I know nothing

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,34 @@
 use super::data::create_graph;
 use yew::prelude::*;
 
+#[derive(Properties, PartialEq)]
+struct NodeViewProps {
+    name: String,
+    index: usize,
+}
+
+#[function_component]
+fn NodeView(props: &NodeViewProps) -> Html {
+    html! {
+        <g transform={format!("translate(200, {})", (props.index + 1) * 150)}>
+            <circle r="50" fill="white" />
+            <text y="5" font-size="14" text-anchor="middle">{props.name.clone()}</text>
+        </g>
+    }
+}
+
 #[function_component]
 pub fn App() -> Html {
     let graph = create_graph();
-    let x = graph.nodes.iter().map(|node| {
-        html! {
-            <div>
-                <h1>{node.name}</h1>
-            </div>
-        }
-    });
 
     html! {
-        <main>
-            {html! { for x }}
-        </main>
+        <svg style="width: 100svw; height: 100svh">
+            { graph.nodes
+                .iter()
+                .enumerate()
+                .map(|(i, node)| html! {<NodeView name={node.name} index={i} /> })
+                .collect::<Html>()
+            }
+        </svg>
     }
 }
